a0784ca9beb2d5ea53cb7263c1d95e764a4f581d
KIRTI-GITHUB <33566404+KIRTIRAI-SLIET@users.noreply.github.com>
2026-02-27 17:26:08 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,21 @@
 //your JS code here. If required.
+const openBtn = document.getElementById("openModal");
+const modal = document.getElementById("modal");
+const closeBtn = document.querySelector(".close-modal");
+
+// Open modal
+openBtn.addEventListener("click", () => {
+  modal.classList.add("show");
+});
+
+// Close on button click
+closeBtn.addEventListener("click", () => {
+  modal.classList.remove("show");
+});
+
+// Close on outside click
+window.addEventListener("click", (e) => {
+  if (e.target === modal) {
+    modal.classList.remove("show");
+  }
+});
